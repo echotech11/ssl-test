0b3ecd95f542ff6089ad690bc9f7b76fa156a5d5
echotech11 <info@echotc.com>
2025-02-21 15:24:15 -0500

    test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, JsonPipe],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  http = inject(HttpClient);
+  res ={};
+
+  ngOnInit(): void {
+
+  }
+
+  onClick() {
+    this.http.get(`https://192.168.20.93`).subscribe({
+      next: (res) => this.onSuccess(res),
+      error: (err) => this.onFailure(err)
+    })
+  }
+
+
+  onSuccess(res: any) {
+    console.log('success', res);
+    this.res = res
+  }
+
+  onFailure(err: any) {
+    console.log('failure', err);
+    this.res = err
+  }
 }
